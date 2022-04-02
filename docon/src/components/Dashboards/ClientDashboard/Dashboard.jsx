0da3d6a88b439f56f12a783cee9d72ed917dd6e4
Joel Martinez <joel.martinez@rutgers.edu>
2022-04-02 17:39:04 -0400

    added request and dashbaord

--- docon/src/components/Dashboards/ClientDashboard/Dashboard.jsx
+++ docon/src/components/Dashboards/ClientDashboard/Dashboard.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import UserNavBar from './UserNavBar'
+import Calendar from 'react-calendar'
 
 var curr_user = window.sessionStorage.getItem("CurrUser")
 var curr_user_json = JSON.parse(curr_user)
 
 export const Dashboard = () => {
     return (
-        <Container> 
-            <UserNavBar/>
+        <Container>
+            <UserNavBar />
+            <div class='grid grid-cols-3 pt-10'>
+                <div class='col-span-2'>
+                    <h3>Overview of 'user'</h3> 
+                </div>
+                <div>
+                    <h3>Appointments</h3>
+                    <Calendar />
+                </div>
+            </div>
         </Container>
     )
 }
--- docon/src/components/Dashboards/ClientDashboard/RequestDoctor/Form.jsx
+++ docon/src/components/Dashboards/ClientDashboard/RequestDoctor/Form.jsx
@@ -7,6 +7,9 @@ export const Form = () => {
             <div align='center'>
                 <h3 class='font-railway'>Please Input Your Doctor Request Issue</h3>
                 <textarea class="w-100 h-80 border-4 outline-black-100 outline-10 hover:outline-2 resize rounded-md caret-black"></textarea>
+                <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                    <a href='#'>Contact Doctor</a>
+                </button>
             </div>
         </Container>
     )
