c6f7868ea39d5a0e6b66403e587a38b859366a63
Joel Martinez <joel.martinez@rutgers.edu>
2022-04-03 02:53:23 -0400

    edit textfield bug

--- docon/src/components/Dashboards/ClientDashboard/ViewHospitals/TextField/TextField.jsx
+++ docon/src/components/Dashboards/ClientDashboard/ViewHospitals/TextField/TextField.jsx
@@ -14,7 +14,7 @@ export const TextField = () => {
         }
         getHospitalDataNow()
     }, [])
-    console.log(data)
+
     // pushes data to hospitals containing JSON objects
     const hospitals = []
     const consolidateJSON = () => {
@@ -29,47 +29,22 @@ export const TextField = () => {
     }
     consolidateJSON()
     // populates array with hospital data JSONs
-    console.log(hospitals[0])
-    const totalRows = []
-    const hospitalNames = []
-    const hospitalAddress = []
-    const hospitalTelephone = []
-    const hospitalLocationLink = []
+    let totalRows = []
     const createTableRows = async () => {
-        if (hospitals.length != 0) {
+        if (hospitals.length !== 0) {
             for (const key in Object.keys(hospitals[0])) {
-                const tempArray = []
-                // console.log('hereree')
-                // console.log(hospitals)
                 const hospitalValue = hospitals[0][key]
-                // console.log(hospitalValue)
-                // hospitalNames.push(hospitalValue.NAME)
                 let baseLink = `https://www.google.com/maps/search/?api=1&query=${hospitalValue.NAME}`
-                // hospitalLocationLink.push(baseLink)
-                // hospitalAddress.push(hospitalValue.ADDRESS)
-                // hospitalTelephone.push(hospitalValue.TELEPHONE)
                 totalRows.push({ "name": hospitalValue.NAME })
                 totalRows.push({ "link": baseLink })
                 totalRows.push({ "addr": hospitalValue.ADDRESS })
                 totalRows.push({ "tele": hospitalValue.TELEPHONE })
-                // totalRows.push.apply(totalRows, tempArray)
-                console.log('checking rn')
-                console.log(totalRows)
-                // tempArray.clear()
             }
         }
     }
-    console.log('main array')
-    console.log(totalRows)
 
     createTableRows()
-    console.log(hospitalNames)
-    console.log(hospitalLocationLink)
-    console.log(hospitalAddress)
-    console.log(hospitalTelephone)
-
     const returnDynamicTable = () => {
-        // if ((hospitalNames.length !== 0) && (hospitalLocationLink.length !== 0) && (hospitalAddress.length !== 0) && (hospitalTelephone.length !== 0)) {
         if (totalRows.length !== 0) {
             return (
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -98,15 +73,14 @@ export const TextField = () => {
                                 totalRows.map(
                                     (item, index) => (
                                         <tr>
-                                            <td key={index + 1}>{item.name}</td>
-                                            <td key={index + 2 }>{item.link}</td>
-                                            <td key={index + 3}>{item.addr}</td>
-                                            <td key={index + 4}>{item.tele}</td>
+                                            <td key={index + 1} ><h6 class='pl-8 pt-4'>{item.name}</h6></td>
+                                            <td key={index + 2}><a href={item.link} target="_blank" class='pl-8'>Link!</a></td>
+                                            <td key={index + 3} class='pl-8'>{item.addr}</td>
+                                            <td key={index + 4} class='pb-8'>{item.tele}</td>
                                         </tr>
                                     )
                                 )
                             }
-
                         </tbody>
                     </table>
                 </div>
@@ -114,7 +88,7 @@ export const TextField = () => {
 
         }
         return (
-            <h1>fail</h1>
+            <h1>Unavailable Location</h1>
         )
     }
 
