9da421349e05c40bb2b7f26bed79b8e06968a067
Karun Kanda <kkanda900@gmail.com>
2022-04-02 23:27:12 -0400

    update

--- docon/src/components/Dashboards/DoctorDashboard/AdminContent/Content.jsx
+++ docon/src/components/Dashboards/DoctorDashboard/AdminContent/Content.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import Calendar from 'react-calendar'
 import { Chart } from "react-google-charts";
 
+var curr_user = window.sessionStorage.getItem("CurrUser")
+var curr_user_json = JSON.parse(curr_user)
+console.log(curr_user_json)
+
 export const data_1 = [
     ["Date", "Patient Requests"],
     ["4/2/22", 3],
@@ -20,11 +24,11 @@ export const data = [
     ["Gender", "Number of Patients"],
     ["Female", 110],
     ["Male", 20]
-  ];
-  
-  export const options = {
+];
+
+export const options = {
     title: "Patients Gender",
-  };
+};
 
 export const Content = () => {
     const [value, onChange] = useState(new Date());
@@ -34,7 +38,7 @@ export const Content = () => {
         <div class='grid grid-cols-3'>
             <div class='col-span-2'>
                 <h3>Overview Of Patients</h3>
-                <Chart
+                {/* <Chart
                     chartType="LineChart"
                     width="100%"
                     height="400px"
@@ -47,7 +51,28 @@ export const Content = () => {
                     options={options}
                     width={"100%"}
                     height={"400px"}
-                />
+                /> */}
+                {curr_user_json.patient ? (
+                <>
+                    <Chart
+                        chartType="LineChart"
+                        width="100%"
+                        height="400px"
+                        data={data_1}
+                        options={options_1}
+                    />
+                    <Chart
+                        chartType="PieChart"
+                        data={data}
+                        options={options}
+                        width={"100%"}
+                        height={"400px"}
+                    />{/*  */}
+                </>
+                ): (
+                    <>
+                    </>
+                )}
             </div>
             <div>
                 <h3>Scheduling</h3>
